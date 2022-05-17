5a08544351a783cc9cfd4ececc744d232f4fd310
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-05-17 10:20:30 +0300

    feat(app): add styles and theme

--- Formula1/Formula1/Formula1/App.xaml.cs
+++ Formula1/Formula1/Formula1/App.xaml.cs
@@ -12,6 +12,11 @@ using Xamarin.Forms;
 [assembly: ExportFont("FontAwesome-Regular.ttf", Alias = "FontAwesome_Regular")]
 [assembly: ExportFont("FontAwesome-Solid.ttf", Alias = "FontAwesome_Solid")]
 
+[assembly: ExportFont("Exo-Black.ttf", Alias = "Exo_Black")]
+[assembly: ExportFont("Exo-Bold.ttf", Alias = "Exo_Bold")]
+[assembly: ExportFont("Exo-Medium.ttf", Alias = "Exo_Medium")]
+[assembly: ExportFont("Exo-Regular.ttf", Alias = "Exo_Regular")]
+
 namespace Formula1
 {
     public partial class App : PrismApplication
