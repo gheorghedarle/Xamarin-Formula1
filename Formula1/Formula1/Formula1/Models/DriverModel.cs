76af3c728847df5e7b49e0391399064427657dd7
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-05-20 17:19:45 +0300

    wip(drivers): wip drivers list

---
+++ Formula1/Formula1/Formula1/Models/DriverModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class DriverModel
+    {
+        public int Position { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public double Points { get; set; }
+        public string Image { get; set; }
+    }
+}
--- Formula1/Formula1/Formula1/ViewModels/TabViews/DriversViewModel.cs
+++ Formula1/Formula1/Formula1/ViewModels/TabViews/DriversViewModel.cs
@@ -1,11 +1,18 @@
-﻿using Formula1.Views;
+﻿using Formula1.Models;
+using Formula1.Views;
 using Prism.Navigation;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace Formula1.ViewModels.TabViews
 {
     public class DriversViewModel : BaseViewModel
     {
+        #region Commands
+
+        public ObservableCollection<DriverModel> DriversList { get; set; }
+
+        #endregion
 
         #region Commands
 
@@ -20,6 +27,34 @@ namespace Formula1.ViewModels.TabViews
             DriverDetailsCommand = new Command(DriverDetailsCommandHandler);
 
             Title = "Drivers";
+
+            DriversList = new ObservableCollection<DriverModel>()
+            {
+                new DriverModel()
+                {
+                    Position = 1,
+                    Name = "Charles LECLERC",
+                    Team = "Ferrari",
+                    Points = 104,
+                    Image = "https://www.formula1.com/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/2col/image.png"
+                },
+                new DriverModel()
+                {
+                    Position = 2,
+                    Name = "Max VERSTAPPEN",
+                    Team = "Red Bull Racing",
+                    Points = 90,
+                    Image = "https://www.formula1.com/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/2col/image.png"
+                },
+                new DriverModel()
+                {
+                    Position = 3,
+                    Name = "George RUSSEL",
+                    Team = "Mercedes",
+                    Points = 80,
+                    Image = "https://www.formula1.com/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png.transform/2col/image.png"
+                }
+            };
         }
 
         #endregion
