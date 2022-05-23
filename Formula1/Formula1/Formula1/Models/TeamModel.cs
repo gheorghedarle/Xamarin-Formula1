fa2b15cdf9a654a208cfaa9564f228bd1984d204
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-05-23 17:00:49 +0300

    wip(teams): add teams list

---
+++ Formula1/Formula1/Formula1/Models/TeamModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class TeamModel
+    {
+        public int Position { get; set; }
+        public string Name { get; set; }
+        public double Points { get; set; }
+    }
+}
--- Formula1/Formula1/Formula1/ViewModels/TabViews/TeamsViewModel.cs
+++ Formula1/Formula1/Formula1/ViewModels/TabViews/TeamsViewModel.cs
@@ -1,14 +1,44 @@
-﻿using Prism.Navigation;
+﻿using Formula1.Models;
+using Prism.Navigation;
+using System.Collections.ObjectModel;
 
 namespace Formula1.ViewModels.TabViews
 {
     public class TeamsViewModel: BaseViewModel
     {
+        #region Properties
+
+        public ObservableCollection<TeamModel> TeamsList { get; set; }
+
+        #endregion
+
         #region Constructors
 
         public TeamsViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "Teams";
+
+            TeamsList = new ObservableCollection<TeamModel>()
+            {
+                new TeamModel()
+                {
+                    Position = 1,
+                    Name = "Red Bull",
+                    Points = 195,
+                },
+                new TeamModel()
+                {
+                    Position = 2,
+                    Name = "Ferrari",
+                    Points = 169,
+                },
+                new TeamModel()
+                {
+                    Position = 3,
+                    Name = "Mercedes",
+                    Points = 120,
+                }
+            };
         }
 
         #endregion
