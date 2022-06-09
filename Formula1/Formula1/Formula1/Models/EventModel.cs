b53d75720c143afc46f851c6ab4866055a47f779
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-06-09 17:18:40 +0300

    wip(schedule): add more info to the model

---
+++ Formula1/Formula1/Formula1/Models/EventModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class EventModel
+    {
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
--- Formula1/Formula1/Formula1/Models/ScheduleModel.cs
+++ Formula1/Formula1/Formula1/Models/ScheduleModel.cs
@@ -1,10 +1,18 @@
-﻿namespace Formula1.Models
+﻿using System;
+
+namespace Formula1.Models
 {
     public class ScheduleModel
     { 
         public int Round { get; set; }
         public string RaceName { get; set; }
         public CircuitModel Circuit { get; set; }
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public EventModel FirstPractice { get; set; }
+        public EventModel SecondPractice { get; set; }
+        public EventModel ThirdPractice { get; set; }
+        public EventModel Qualifying { get; set; }
+        public EventModel Sprint { get; set; }
     }
 }
