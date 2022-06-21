3ebdbe2822c336c979dffc0651029dfffe69c189
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-06-21 10:53:36 +0300

    fix(shell): fix bottom nav bar

---
+++ Formula1/Formula1/Formula1.Android/Renderers/ShellCustomRenderer.cs
@@ -0,0 +1,66 @@
+﻿using Android.Content;
+using Android.Widget;
+using Formula1;
+using Formula1.Droid.Renderers;
+using Google.Android.Material.BottomNavigation;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(AppShell), typeof(ShellCustomRenderer))]
+namespace Formula1.Droid.Renderers
+{
+    public class ShellCustomRenderer : ShellRenderer
+
+    {
+        public ShellCustomRenderer(Context context) : base(context)
+        { }
+
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return new MarginedTabBarAppearance();
+        }
+    }
+
+    public class ToolbarAppearance : IShellToolbarAppearanceTracker
+    {
+        public void Dispose()
+        { }
+
+        public void ResetAppearance(Toolbar toolbar, IShellToolbarTracker toolbarTracker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetAppearance(AndroidX.AppCompat.Widget.Toolbar toolbar, IShellToolbarTracker toolbarTracker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAppearance(Toolbar toolbar, IShellToolbarTracker toolbarTracker, ShellAppearance appearance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAppearance(AndroidX.AppCompat.Widget.Toolbar toolbar, IShellToolbarTracker toolbarTracker, ShellAppearance appearance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MarginedTabBarAppearance : IShellBottomNavViewAppearanceTracker
+    {
+        public void Dispose()
+        { }
+
+        public void ResetAppearance(BottomNavigationView bottomView)
+        { }
+
+
+        public void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
+        {
+            bottomView.LabelVisibilityMode = LabelVisibilityMode.LabelVisibilityUnlabeled;
+        }
+    }
+}
---
+++ Formula1/Formula1/Formula1.iOS/Renderers/ShellCustomRenderer.cs
@@ -0,0 +1,53 @@
+﻿using Formula1;
+using Formula1.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(AppShell), typeof(ShellCustomRenderer))]
+namespace Formula1.iOS.Renderers
+{
+    public class ShellCustomRenderer : ShellRenderer
+    {
+        protected override IShellSectionRenderer CreateShellSectionRenderer(ShellSection shellSection)
+        {
+            var renderer = base.CreateShellSectionRenderer(shellSection);
+            if (renderer != null)
+            {
+
+            }
+            return renderer;
+        }
+
+        protected override IShellTabBarAppearanceTracker CreateTabBarAppearanceTracker()
+        {
+            return new CustomTabbarAppearance();
+        }
+    }
+
+    public class CustomTabbarAppearance : IShellTabBarAppearanceTracker
+    {
+        public void Dispose()
+        { }
+
+        public void ResetAppearance(UITabBarController controller)
+        { }
+
+        public void SetAppearance(UITabBarController controller, ShellAppearance appearance)
+        {
+            UITabBar myTabBar = controller.TabBar;
+
+            if (myTabBar.Items != null)
+            {
+                foreach (UITabBarItem item in myTabBar.Items)
+                {
+                    item.Title = null;
+                    item.ImageInsets = new UIEdgeInsets(10, 0, 0, 0);
+                }
+            }
+        }
+
+        public void UpdateLayout(UITabBarController controller)
+        { }
+    }
+}
