91a06052c340f85655d9f75e4183b2a2cebbd41a
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-07-07 16:14:04 +0300

    feat(results): add empty results page

---
+++ Formula1/Formula1/Formula1/Views/Fragments/EmptyRaceResults.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula1.Views.Fragments
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmptyRaceResults : StackLayout
+    {
+        public EmptyRaceResults()
+        {
+            InitializeComponent();
+        }
+    }
+}
