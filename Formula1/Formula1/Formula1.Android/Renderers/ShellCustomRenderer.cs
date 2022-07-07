b9f58a540aa8bd71341fce8966c5fc863d29a689
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-07-07 13:49:14 +0300

    fix(tab): fix tab color and theme

--- Formula1/Formula1/Formula1.Android/Renderers/ShellCustomRenderer.cs
+++ Formula1/Formula1/Formula1.Android/Renderers/ShellCustomRenderer.cs
@@ -1,4 +1,6 @@
 ﻿using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
 using Android.Widget;
 using Formula1;
 using Formula1.Droid.Renderers;
@@ -7,11 +9,13 @@ using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
+using AColor = Android.Graphics.Color;
+using R = Android.Resource;
+
 [assembly: ExportRenderer(typeof(AppShell), typeof(ShellCustomRenderer))]
 namespace Formula1.Droid.Renderers
 {
     public class ShellCustomRenderer : ShellRenderer
-
     {
         public ShellCustomRenderer(Context context) : base(context)
         { }
@@ -19,48 +23,161 @@ namespace Formula1.Droid.Renderers
 
         protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
         {
-            return new MarginedTabBarAppearance();
+            return new ShellBottomNavViewAppearanceTracker(this, shellItem);
         }
-    }
+	}
 
-    public class ToolbarAppearance : IShellToolbarAppearanceTracker
-    {
-        public void Dispose()
-        { }
+	public class ShellBottomNavViewAppearanceTracker : IShellBottomNavViewAppearanceTracker
+	{
+		IShellContext _shellContext;
+		ShellItem _shellItem;
+		ColorStateList _defaultList;
+		bool _disposed;
+		ColorStateList _colorStateList;
 
-        public void ResetAppearance(Toolbar toolbar, IShellToolbarTracker toolbarTracker)
-        {
-            throw new NotImplementedException();
-        }
+		public ShellBottomNavViewAppearanceTracker(IShellContext shellContext, ShellItem shellItem)
+		{
+			_shellItem = shellItem;
+			_shellContext = shellContext;
+		}
 
-        public void ResetAppearance(AndroidX.AppCompat.Widget.Toolbar toolbar, IShellToolbarTracker toolbarTracker)
-        {
-            throw new NotImplementedException();
-        }
+		public virtual void ResetAppearance(BottomNavigationView bottomView)
+		{
+			if (_defaultList != null)
+			{
+				bottomView.ItemTextColor = _defaultList;
+				bottomView.ItemIconTintList = _defaultList;
+			}
 
-        public void SetAppearance(Toolbar toolbar, IShellToolbarTracker toolbarTracker, ShellAppearance appearance)
-        {
-            throw new NotImplementedException();
-        }
+			SetBackgroundColor(bottomView, Color.White);
+		}
 
-        public void SetAppearance(AndroidX.AppCompat.Widget.Toolbar toolbar, IShellToolbarTracker toolbarTracker, ShellAppearance appearance)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public virtual void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
+		{
+			IShellAppearanceElement controller = appearance;
+			var backgroundColor = controller.EffectiveTabBarBackgroundColor;
+			var foregroundColor = controller.EffectiveTabBarForegroundColor; // currently unused
+			var disabledColor = controller.EffectiveTabBarDisabledColor;
+			var unselectedColor = controller.EffectiveTabBarUnselectedColor;
+			var titleColor = controller.EffectiveTabBarTitleColor;
 
-    public class MarginedTabBarAppearance : IShellBottomNavViewAppearanceTracker
-    {
-        public void Dispose()
-        { }
+			if (_defaultList == null)
+			{
+#if __ANDROID_28__
+				_defaultList = bottomView.ItemTextColor ?? bottomView.ItemIconTintList
+					?? MakeColorStateList(titleColor.ToAndroid().ToArgb(), disabledColor.ToAndroid().ToArgb(), unselectedColor.ToAndroid().ToArgb());
+#else
+				_defaultList = bottomView.ItemTextColor ?? bottomView.ItemIconTintList;
+#endif
+			}
 
-        public void ResetAppearance(BottomNavigationView bottomView)
-        { }
+			_colorStateList = MakeColorStateList(titleColor, disabledColor, unselectedColor);
+			bottomView.ItemTextColor = _colorStateList;
+			bottomView.ItemIconTintList = _colorStateList;
 
+			SetBackgroundColor(bottomView, backgroundColor);
+			bottomView.LabelVisibilityMode = LabelVisibilityMode.LabelVisibilityUnlabeled;
+		}
 
-        public void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
-        {
-            bottomView.LabelVisibilityMode = LabelVisibilityMode.LabelVisibilityUnlabeled;
-        }
-    }
+		protected virtual void SetBackgroundColor(BottomNavigationView bottomView, Color color)
+		{
+			var menuView = bottomView.GetChildAt(0) as BottomNavigationMenuView;
+			var oldBackground = bottomView.Background;
+			var colorDrawable = oldBackground as ColorDrawable;
+			var colorChangeRevealDrawable = oldBackground as ColorChangeRevealDrawable;
+			AColor lastColor = colorDrawable?.Color ?? Color.Default.ToAndroid();
+			AColor newColor;
+
+			if (color == Color.Default)
+				newColor = Color.White.ToAndroid();
+			else
+				newColor = color.ToAndroid();
+
+			if (menuView == null)
+			{
+				if (colorDrawable != null && lastColor == newColor)
+					return;
+
+				if (lastColor != newColor || colorDrawable == null)
+				{
+					bottomView.SetBackground(new ColorDrawable(newColor));
+				}
+			}
+			else
+			{
+				if (colorChangeRevealDrawable != null && lastColor == newColor)
+					return;
+
+				var index = ((IShellItemController)_shellItem).GetItems().IndexOf(_shellItem.CurrentItem);
+				var menu = bottomView.Menu;
+				index = Math.Min(index, menu.Size() - 1);
+
+				var child = menuView.GetChildAt(index);
+				if (child == null)
+					return;
+
+				var touchPoint = new Point(child.Left + (child.Right - child.Left) / 2, child.Top + (child.Bottom - child.Top) / 2);
+
+				bottomView.SetBackground(new ColorChangeRevealDrawable(lastColor, newColor, touchPoint));
+			}
+		}
+
+		ColorStateList MakeColorStateList(Color titleColor, Color disabledColor, Color unselectedColor)
+		{
+			var disabledInt = disabledColor.IsDefault ?
+				_defaultList.GetColorForState(new[] { -R.Attribute.StateEnabled }, AColor.Gray) :
+				disabledColor.ToAndroid().ToArgb();
+
+			var checkedInt = titleColor.IsDefault ?
+				_defaultList.GetColorForState(new[] { R.Attribute.StateChecked }, AColor.Black) :
+				titleColor.ToAndroid().ToArgb();
+
+			var defaultColor = unselectedColor.IsDefault ?
+				_defaultList.DefaultColor :
+				unselectedColor.ToAndroid().ToArgb();
+
+			return MakeColorStateList(checkedInt, disabledInt, defaultColor);
+		}
+
+		ColorStateList MakeColorStateList(int titleColorInt, int disabledColorInt, int defaultColor)
+		{
+			var states = new int[][] {
+				new int[] { -R.Attribute.StateEnabled },
+				new int[] {R.Attribute.StateChecked },
+				new int[] { }
+			};
+
+			var colors = new[] { disabledColorInt, titleColorInt, defaultColor };
+
+			return new ColorStateList(states, colors);
+		}
+
+		#region IDisposable
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (_disposed)
+				return;
+
+			_disposed = true;
+
+			if (disposing)
+			{
+				_defaultList?.Dispose();
+				_colorStateList?.Dispose();
+
+				_shellItem = null;
+				_shellContext = null;
+				_defaultList = null;
+				_colorStateList = null;
+			}
+		}
+
+		#endregion IDisposable
+	}
 }
