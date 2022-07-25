d7c2cbca65843f014d7ff4e0de8343619f9fb289
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-07-25 11:17:16 +0300

    update(ui): update UI and small fix

--- Formula1-Informations-API/index.js
+++ Formula1-Informations-API/index.js
@@ -6,10 +6,7 @@ var express = require("express");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-// var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
-// var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
-
-// var credentials = { key: privateKey, cert: certificate };
+const helpers = require("./helpers/helpers");
 
 var baseUrl = "https://www.formula1.com/en";
 var app = express();
@@ -27,7 +24,7 @@ app.get("/driver/info", async function (req, res) {
         ).each((index, element) => {
           const label = $(element).find("th > span").text();
           const value = $(element).find("td").text();
-          r[camelize(label)] = value;
+          r[helpers.camelize(label)] = value;
         });
         var finalResult = { result: r };
         res.status(200).end(JSON.stringify(finalResult));
@@ -43,7 +40,7 @@ app.get("/team/info", async function (req, res) {
   if (req.query.team != null) {
     console.log(`/team/info - ${req.query.team}`);
     axios
-      .get(`${baseUrl}/teams/${convertToTeamName(req.query.team)}.html`)
+      .get(`${baseUrl}/teams/${helpers.convertToTeamName(req.query.team)}.html`)
       .then((result) => {
         var r = {};
         const $ = cheerio.load(result.data);
@@ -52,7 +49,7 @@ app.get("/team/info", async function (req, res) {
         ).each((index, element) => {
           const label = $(element).find("th > span").text();
           const value = $(element).find("td").text();
-          r[camelize(label)] = value;
+          r[helpers.camelize(label)] = value;
         });
         var finalResult = { result: r };
         res.status(200).end(JSON.stringify(finalResult));
@@ -77,7 +74,7 @@ app.get("/circuit/info", async function (req, res) {
         ).each((index, element) => {
           const label = $(element).find("div > p.misc--label").text();
           const value = $(element).find("div > p.f1-bold--stat").text();
-          r[camelize(label)] = value;
+          r[helpers.camelize(label)] = value;
         });
         var finalResult = { result: r };
         res.status(200).end(JSON.stringify(finalResult));
@@ -89,40 +86,8 @@ app.get("/circuit/info", async function (req, res) {
   }
 });
 
-function camelize(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-    if (+match === 0) return "";
-    return match.toUpperCase();
-  });
-}
-
-function convertToTeamName(team) {
-  switch (team) {
-    case "alfa":
-      return "Alfa-Romeo-Racing";
-    case "alphatauri":
-      return "AlphaTauri";
-    case "alpine":
-      return "Alpine";
-    case "aston_martin":
-      return "Aston-Martin";
-    case "ferrari":
-      return "Ferrari";
-    case "haas":
-      return "Haas-F1-Team";
-    case "mclaren":
-      return "McLaren";
-    case "mercedes":
-      return "Mercedes";
-    case "red_bull":
-      return "Red-Bull-Racing";
-    case "williams":
-      return "Williams";
-  }
-}
-
 var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(credentials, app);
-
 httpServer.listen(port);
+
+// var httpsServer = https.createServer(credentials, app);
 // httpsServer.listen(securePort);
