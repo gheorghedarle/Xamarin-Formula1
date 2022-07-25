33c30ab5cf55aca6adab3170cf9e48e014c3e6fa
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-07-25 13:16:13 +0300

    refactor(driver-informations): add informations template

---
+++ Formula1/Formula1/Formula1/Views/Templates/InformationDateOfBirthTemplate.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula1.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InformationDateOfBirthTemplate : StackLayout
+    {
+        public InformationDateOfBirthTemplate()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(InformationTemplate), string.Empty);
+        public static readonly BindableProperty DateOfBirthProperty = BindableProperty.Create(nameof(DateOfBirth), typeof(string), typeof(InformationTemplate), string.Empty);
+        public static readonly BindableProperty AgeProperty = BindableProperty.Create(nameof(Age), typeof(string), typeof(InformationTemplate), string.Empty);
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public string DateOfBirth
+        {
+            get => (string)GetValue(DateOfBirthProperty);
+            set => SetValue(DateOfBirthProperty, value);
+        }
+
+        public string Age
+        {
+            get => (string)GetValue(AgeProperty);
+            set => SetValue(AgeProperty, value);
+        }
+    }
+}
---
+++ Formula1/Formula1/Formula1/Views/Templates/InformationTemplate.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula1.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InformationTemplate : StackLayout
+    {
+        public InformationTemplate()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(InformationTemplate), string.Empty);
+        public static readonly BindableProperty ValueProperty = BindableProperty.Create(nameof(Value), typeof(string), typeof(InformationTemplate), string.Empty);
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(LabelProperty, value);
+        }
+    }
+}
