b679e0f0fbc36a59327f14919a11c8bbb7dcdddf
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-07-27 15:27:12 +0300

    feat(android): add splash screen

--- Formula1/Formula1/Formula1.Android/Resources/Resource.designer.cs
+++ Formula1/Formula1/Formula1.Android/Resources/Resource.designer.cs
@@ -22239,19 +22239,28 @@ namespace Formula1.Droid
 			public const int preference_list_divider_material = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int team = 2131165329;
+			public const int splash_background = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int test_custom_background = 2131165330;
+			public const int splash_logo = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int tooltip_frame_dark = 2131165331;
+			public const int splash_screen = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int tooltip_frame_light = 2131165332;
+			public const int team = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int welcome = 2131165333;
+			public const int test_custom_background = 2131165333;
+			
+			// aapt resource value: 0x7F070096
+			public const int tooltip_frame_dark = 2131165334;
+			
+			// aapt resource value: 0x7F070097
+			public const int tooltip_frame_light = 2131165335;
+			
+			// aapt resource value: 0x7F070098
+			public const int welcome = 2131165336;
 			
 			static Drawable()
 			{
