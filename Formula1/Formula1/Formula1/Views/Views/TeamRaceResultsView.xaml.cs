fe95f2971af64bddce3ed5b1793c6fce57c4e884
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-07-29 11:49:25 +0300

    feat(loading): add team race results

---
+++ Formula1/Formula1/Formula1/Views/Views/TeamRaceResultsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula1.Views.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TeamRaceResultsView : Grid
+    {
+        public TeamRaceResultsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
